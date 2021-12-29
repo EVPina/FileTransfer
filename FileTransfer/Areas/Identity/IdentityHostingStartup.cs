79f4c37e98b1d129f201e0b332b78eb35831eee7
Enzo Vargas <enzo.vp@outlook.com>
2021-12-28 21:05:05 -0500

    Se agrego identity

---
+++ FileTransfer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FileTransfer.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FileTransfer.Areas.Identity.IdentityHostingStartup))]
+namespace FileTransfer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FileTransferContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Conexion")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FileTransferContext>();
+            });
+        }
+    }
+}
