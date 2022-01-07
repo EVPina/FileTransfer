673d8d82a3e4e5bdac531cc0e4df28dc555faef2
Enzo Vargas <enzo.vp@outlook.com>
2022-01-07 16:56:18 -0500

    add profile

---
+++ FileTransfer/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using FileTransfer.Models.Entities;
+using FileTransfer.ViewsModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileTransfer.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Profile(VMProfile vMProfile)
+        {
+            return View(vMProfile);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendProfile(VMProfile vMProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Profile));
+            }
+            return View(vMProfile);
+        }
+    }
+}
---
+++ FileTransfer/ViewsModels/VMProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileTransfer.ViewsModels
+{
+    public class VMProfile : IListErros
+    {
+        [DataType(DataType.EmailAddress)]
+        [Display( Name = "Email")]
+        public string Correo { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display( Name = "Password")]
+        public string Contraseña { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Compare(nameof(Contraseña), ErrorMessage = "Contraseñas diferentes")]
+        [Display( Name = "Confirm Password")]
+        public string ConfirmContraseña { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display( Name = "Mobile Phone")]
+        public string Celular { get; set; }
+
+
+        public List<dynamic> ListErros { get; set; }
+    }
+}
