49a61ab2108fd8cbc1af1781f54f5c22fc8ea93f
Enzo Vargas <enzo.vp@outlook.com>
2022-03-01 22:47:23 -0500

    Se agrego vista archivos

--- FileTransfer/Controllers/HomeController.cs
+++ FileTransfer/Controllers/HomeController.cs
@@ -23,16 +23,10 @@ namespace FileTransfer.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        
+        public IActionResult Archivos()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
